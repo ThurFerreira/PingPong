a60d64dd3b8d3db78097b886e7efd252beccd29b
Arthur <arthurfsilva110@gmail.com>
2022-04-20 13:41:15 -0300

    Game de ping pong desenvolvido em java

---
+++ src/Player.java
@@ -0,0 +1,36 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Player {
+
+	public boolean right,left;
+	public int x,y;
+	public int width, height;
+	public double speed = 500;
+	
+	public Player(int x, int y) {
+		this.x = x;
+		this.y = y;
+		this.width = 40;
+		this.height = 5;
+	}
+	
+	 public void tick() {
+		 if(right) {
+			 x++;
+		 }else if(left) {
+			 x--;
+		 }
+		 
+		 if(x+width > Game.WIDTH) {
+		 x = Game.WIDTH - width;		 
+	 }else if(x < 0) {
+		 x = 0;
+	 }
+	}
+	 public void render(Graphics g) {
+		 g.setColor(new Color(255,165,0));
+			g.fillRect(x,y,width, height);
+	 }
+
+}
